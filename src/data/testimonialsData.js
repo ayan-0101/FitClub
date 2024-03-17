9824bdc3c07187221a3cb372d9e22aced207b00f
Mohd. Ayan <ayanshakeel02@gmail.com>
2024-03-17 15:44:24 +0530

    Fix: update the data in testimonialsData

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -8,17 +8,17 @@ export const testimonialsData = [
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
     name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    status: 'ENTREPRENEUR'
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
+    review: "It was really a great experience for me with FitClub. It was really helpful for me. It's worth a purchase",
     name: 'JOHN KEVIN',
     status: 'COACH'
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
+    image: image3,
+    review: "All the Trainers are really helpful and also the equipnents are modern and user-friendly. I am very satisfy with my purchase",
     name: 'FRANKLIN',
     status: "CUSTOMER"
   }
